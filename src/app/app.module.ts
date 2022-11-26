02066f1a513fafdc50f6b00fc876cf3010b5315a
GabrielHSS <gabrielbet4@gmail.com>
2022-11-26 14:44:19 -0300

    feat: Criação dos diretórios do componente de pesquisa da cidade e remoção do mesmo do componente home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { SearchCityFormComponent } from './components/search-city-form/search-city-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SearchCityFormComponent
   ],
   imports: [
     BrowserModule
