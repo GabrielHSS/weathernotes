1ce66a3b21aba5dcb6fed6732e63ff502b3c3ed2
GabrielHSS <gabrielbet4@gmail.com>
2022-11-27 20:13:02 -0300

    feat: Adição de roteamento no app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { SearchCityFormComponent } from './components/search-city-form/search-city-form.component';
 import { RecommendedMusicalGenreComponent } from './components/recommended-musical-genre/recommended-musical-genre.component';
+import { SearchCityFormComponent } from './components/search-city-form/search-city-form.component';
+import { MyPlaylistComponent } from './components/my-playlist/my-playlist.component';
+import { CityWeatherInfoComponent } from './components/city-weather-info/city-weather-info.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, SearchCityFormComponent, RecommendedMusicalGenreComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule, ReactiveFormsModule],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SearchCityFormComponent,
+    RecommendedMusicalGenreComponent,
+    MyPlaylistComponent,
+    CityWeatherInfoComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
