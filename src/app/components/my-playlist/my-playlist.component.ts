38e259996b5aa819e993c6faaeff57176f45ba41
GabrielHSS <gabrielbet4@gmail.com>
2022-11-27 20:13:15 -0300

    feat: Criação da página de playlist

---
+++ src/app/components/my-playlist/my-playlist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-playlist',
+  templateUrl: './my-playlist.component.html',
+  styleUrls: ['./my-playlist.component.sass']
+})
+export class MyPlaylistComponent {
+
+}
