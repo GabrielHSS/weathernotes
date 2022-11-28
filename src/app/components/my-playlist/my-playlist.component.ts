be6f2e55ab1a2b3cabb08b6d840456e8e80e9ef2
GabrielHSS <gabrielbet4@gmail.com>
2022-11-28 14:13:20 -0300

    feat: Implementação da remoção das músicas da playlist

--- src/app/components/my-playlist/my-playlist.component.ts
+++ src/app/components/my-playlist/my-playlist.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 import { MusicItem } from 'src/app/types';
 import { getJSONFromlocalStorage } from 'src/app/utils';
 
@@ -7,11 +7,30 @@ import { getJSONFromlocalStorage } from 'src/app/utils';
   templateUrl: './my-playlist.component.html',
   styleUrls: ['./my-playlist.component.sass'],
 })
-export class MyPlaylistComponent implements OnInit {
+export class MyPlaylistComponent implements OnInit, OnChanges {
   playlist: MusicItem[] = [];
+
   ngOnInit(): void {
+    this.handlePlaylistItems();
+  }
+  ngOnChanges(): void {
+    this.handlePlaylistItems();
+  }
+
+  handlePlaylistItems() {
     if (getJSONFromlocalStorage('musicPlaylist')) {
       this.playlist = getJSONFromlocalStorage('musicPlaylist') as MusicItem[];
     }
+  }
+  removeItem(music: MusicItem) {
+    const newPlaylist = getJSONFromlocalStorage('musicPlaylist').filter(
+      (playlistItem) => playlistItem.key !== music.key
+    );
+    localStorage.setItem('musicPlaylist', JSON.stringify(newPlaylist));
+    this.playlist = newPlaylist;
+  }
+  deletePlaylist() {
+    localStorage.removeItem('musicPlaylist');
+    this.playlist = [];
   }
 }
